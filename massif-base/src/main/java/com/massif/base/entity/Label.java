20397cf9548aac54d286d24f5284ea0a2e6e4a34
wangsiyao20 <18333116983@163.com>
2023-07-08 13:23:52 +0800

    massif-base模块springboot整合swagger

--- massif-base/src/main/java/com/massif/base/entity/Label.java
+++ massif-base/src/main/java/com/massif/base/entity/Label.java
@@ -1,17 +1,33 @@
 package com.massif.base.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel("标签")
 public class Label implements Serializable{
 
+    @ApiModelProperty("标签ID")
     private String id;
-    //需要要给labelName一个别名，在使用sql时
+
+    // 数据表中中为labelname
+    @ApiModelProperty("标签名")
     private String labelName;
+
+    @ApiModelProperty("状态")
     private String state;
+
+    // 如果实体类属性类型是Integer， 就把example转为Long类型， 而example默认为""
+    // 所以要给example一个数值型
+    @ApiModelProperty(value = "数量", example = "0")
     private Integer count;
+
+    @ApiModelProperty("是否推荐")
     private String recommend;
+
+    @ApiModelProperty(value = "粉丝数", example = "0")
     private Integer fans;
 }
